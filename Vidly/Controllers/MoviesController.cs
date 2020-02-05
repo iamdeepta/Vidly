32454ed642beb8760291d9df3bd2088c824c586c
iamdeepta <47137605+iamdeepta@users.noreply.github.com>
2020-02-05 17:35:41 +0600

    Update MoviesController.cs

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -14,6 +14,30 @@ namespace Vidly.Controllers
         {
             var movie = new Movie() { Name = "Shrek!" };
             return View(movie);
+            //return Content("Hello World!");
+           // return HttpNotFound();
+           // return new EmptyResult();
+
+            //return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" });
+        }
+
+        public ActionResult Edit(int id) {
+
+            return Content("Id=" + id);
+        }
+
+        //movies
+        public ActionResult Index(int? pageIndex, string sortBy) //? for make it nullable
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
