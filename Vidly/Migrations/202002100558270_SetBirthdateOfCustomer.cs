2f258ea72891e5f73a9f0e884a069c34dc0de779
iamdeepta <47137605+iamdeepta@users.noreply.github.com>
2020-02-10 12:54:31 +0600

    Add customer details

---
+++ Vidly/Migrations/202002100558270_SetBirthdateOfCustomer.cs
@@ -0,0 +1,18 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class SetBirthdateOfCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE Customers SET Birthdate = 1/1/1995 WHERE Id = 4");
+            
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
